213464e035b9b88c6f9e2c1f586b2886fd00e8f0
huoxingbaozi <2277516380@qq.com>
2017-05-22 19:29:33 +0800

    first change

---
+++ app/src/main/java/wangyadi/baway/com/wangyadi/Second.java
@@ -0,0 +1,18 @@
+package wangyadi.baway.com.wangyadi;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * 类的作用：
+ * 类的思路：
+ * 时间：2017/5/22
+ * 作者：王亚迪
+ */
+
+public class Second extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
